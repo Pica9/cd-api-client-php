fd9004eb69be6ba9ea7a3d34bc62c0af6ad1814c
Rob Strong <rstrong@pica9.com>
2016-03-24 17:13:27 -0400

    update version

--- src/Configuration.php
+++ src/Configuration.php
@@ -110,7 +110,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = "PHP-Swagger/1.0.0";
+    protected $userAgent = "PHP-Swagger/1.0.2";
 
     /**
      * Debug switch (default set to false)
@@ -516,8 +516,8 @@ class Configuration
         $report  = "PHP SDK (Pica9\CampaignDrive\ApiClient) Debug Report:\n";
         $report .= "    OS: ".php_uname()."\n";
         $report .= "    PHP Version: ".phpversion()."\n";
-        $report .= "    OpenAPI Spec Version: 0.0.1\n";
-        $report .= "    SDK Package Version: 1.0.0\n";
+        $report .= "    OpenAPI Spec Version: 1.0.0\n";
+        $report .= "    SDK Package Version: 1.0.2\n";
         $report .= "    Temp Folder Path: ".self::getDefaultConfiguration()->getTempFolderPath()."\n";
 
         return $report;
